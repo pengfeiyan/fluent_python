733ebd633817704bbcd51ccce1bfd9e39d81631f
yan.pengfei <yan.pengfei@immomo.com>
2018-11-19 18:02:16 +0800

    第二章

--- 第二章/2.10.py
+++ 第二章/2.10.py
@@ -24,8 +24,12 @@ print(q.get(0))
 
 # queue的task_done和join方法在多线程中很有用
 
-
 # 扁平序列,str,bytes,bytearray,array.array,memoryview都是要求序列元素是同一种类型的
 # 容器序列list,tuple,collections.deque等
 
 
+# 插一嘴，python 3.6及以下的缓存机制，小数字缓存和字符串缓存，数字的[-5,256]和20位字符串会做缓存，python3.7以后缓存机制修改了下
+
+#3.6  i is j == False,   3.7 i is j == True
+i = 257
+j = 258-1
